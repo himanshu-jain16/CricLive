142d21cff0377da216a78da046537ec38882b663
Himanshu Jain <Himanshu Jain>
2016-01-06 03:53:53 -0800

    extracted match data from webpage

--- js/controller.js
+++ js/controller.js
@@ -2,16 +2,42 @@ var cricLive = new angular.module('cricLive',[]);
 
 cricLive.controller('MainController',['$scope','$http','$timeout',function($scope,$http,$timeout){
 
-	$scope.getProductData = function(){
+	$scope.getMatches = function(){
 		 (function tick() {
-	 		$http.get('http://localhost:3001/myendpoint').success(function(response){
-			$scope.asd = response.data;
-			console.log("response from server");
-			console.log(response.data);
-			$timeout(tick, 5000);
+	 		$http.get('http://localhost:3001/getallmatches').success(function(response){
+	 			//console.log(response);
+	 		if(true)
+	 		{
+	 			$scope.matches = response;
+				// score_row = response;
+				// var htmlObject = document.createElement('div');
+				// htmlObject.setAttribute("id", "scorerow");
+				// htmlObject.innerHTML = score_row;
+				// var scoreDiv = document.getElementById("scorerow");
+				// scoreDiv.removeChild(scoreDiv.firstChild);
+				// scoreDiv.appendChild(htmlObject);
+				 $timeout(tick, 1000);
+			}
 		});
 	 		})();
 	};
 
+	$scope.getMatchData = function(selectedmatchip){
+		var selectedmatch = {"matchurl":selectedmatchip};
+		(function tick() {
+	 		$http.post('http://localhost:3001/getmatchdata',selectedmatch).success(function(response){
+			console.log(response);
+	 		if(true)
+	 		{	
+	 			console.log("SERVER NE KYA BHEJA");
+	 			console.log(response);
+				 $timeout(tick, 10000);
+			}
+		});
+	 		})();
+	};
 
 }]);
+
+
+
--- js/main.js
+++ js/main.js
@@ -9,16 +9,24 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 var driver = require('node-phantom-simple');
     
  
-    
-    var scrapeddata = {};
-    url = 'http://www.espncricinfo.com/';
+var matches=[];
+var scrapedMatchData = [];
+// var teamNameAndScores = "";
+// var batsmenAndBowlerStats="";
+// var runsInrecentOver= "";
 
-function updateCache() {
+url = 'http://www.espncricinfo.com/';
+
+function getMatchUrl() {
     request(url, function(error, response, html){
         if(!error){
+            matches = [];
             var $ = cheerio.load(html);
 
             var title, release, rating;
@@ -27,79 +35,77 @@ function updateCache() {
             $('.scoreline-list').first().filter(function(){
                 var data = $(this);
                 var numOfMatches = data.children().length;
-                console.log("Number of Matches:  ",numOfMatches);
-                var matches=[];
+                //console.log("Number of Matches:  ",numOfMatches);
 
                 //GET URL FOR EACH MATCH
                 for(x=0;x<numOfMatches;x++)
                 {
                     var lielem = data.children().eq(x);
                     matches[x] = "http://www.espncricinfo.com" + lielem.children().first().attr('href');
-                    $('#editor').val(matches[x]);
-                    console.log(matches[x]);
+                    //console.log(matches[x]);
 
                 }
-                
-                //FOR EACH MATCH URL
-                for(x=0;x<numOfMatches;x++)
-                {  
-                    var matchurl = matches[x];   
-                    //console.log(matchurl);
-                    driver.create({ path: require('phantomjs').path }, function (err, browser) {
-                      return browser.createPage(function (err, page) {
-                        return page.open(matchurl, function (err,status) {
-                          console.log("opened site? ", status);
-                          page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js', function (err) {
-                            // jQuery Loaded. 
-                            // Wait for a bit for AJAX content to load on the page. Here, we are waiting 5 seconds. 
-                            setTimeout(function () {
-                              return page.evaluate(function () {
-                                //Get what you want from the page using jQuery. A good way is to populate an object with all the jQuery commands that you need and then return the object. 
-                                var h2Arr = [];
-                     
-                                $('.innings-information').each(function () { h2Arr.push($(this).html()); });
-                                scrapeddata = JSON.parse(h2Arr);
-                                return {
-                                  h2: h2Arr
-                                };
-                              }, function (err,result) {
-                                browser.exit();
-                              });
-                            }, 5000);
-                          });
-                          });
-                      });
-                    });
-                 setTimeout(function(){
-                        //waiting for the jquery to load
-                        //scrapeddata = h2Arr;
-
-                    }, 5000);
-
-                } //END FOR LOOP EACH MATCH URL
-
-
-
-
-            })
-
-            // Since the rating is in a different section of the DOM, we'll have to write a new jQuery filter to extract this information.
-
-           
-            //console.log(json);
+            });
         }
-    }); // end of request
+    });
+     setTimeout(function(){
+        //waiting for matches url
+    }, 2000); 
+}
+
+function getMatchData(matchurl) {
+    console.log("url",matchurl);
+    driver.create({ path: require('phantomjs').path }, function (err, browser) {
+      return browser.createPage(function (err, page) {
+        return page.open(matchurl, function (err,status) {
+          console.log("opened site? ", status);
+          page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js', function (err) {
+            // jQuery Loaded. 
+            // Wait for a bit for AJAX content to load on the page. Here, we are waiting 5 seconds. 
+            setTimeout(function () {
+              return page.evaluate(function () {
+                //Get what you want from the page using jQuery. A good way is to populate an object with all the jQuery commands that you need and then return the object. 
+                var inningsinfo = [];
+                var scoretable = [];
+                var recentovers = [];
+     
+                $('.innings-information').each(function () { inningsinfo.push($(this).html()); });
+                $('.score-table').each(function () { scoretable.push($(this).html()); });
+                $('.recent-overs').each(function () { recentovers.push($(this).html()); });
+                console.log("inningsinfo",inningsinfo);
+
+                return{innings_info:inningsinfo,
+                        score_table:scoretable,
+                        recent_overs:recentovers};
+              }, function (err,result) {
+                scrapedMatchData = result;
+                browser.exit();
+              });
+            }, 10000);
+          });
+          });
+      });
+    });
+ setTimeout(function(){
+        //waiting for jquery to load
+    }, 5000);    
 }
 
 //Update cache every 60 secs.
-setInterval(updateCache, 10000);
+//setInterval(getMatchUrl, 10000);
                             
 
-app.get('/myendpoint', function(req, res) {
-    console.log("GET scrapeddata");
-    console.log(scrapeddata);
-    res.json(scrapeddata);
-        })
+app.get('/getallmatches', function(req, res) {
+    getMatchUrl();
+    res.json(matches);
+});
+
+app.post('/getmatchdata', function(req, res) {
+    console.log("KONSA MATCH");
+    var matchurl = req.body.matchurl;
+    getMatchData(matchurl);
+    res.json(scrapedMatchData);
+});
 
 
 menu.append(new gui.MenuItem({
