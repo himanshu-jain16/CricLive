de6d2850506b4f35b9f1ed78a83ff32230c19363
Himanshu Jain <Himanshu Jain>
2016-01-14 18:22:19 -0800

    completed all basic functions

--- js/controller.js
+++ js/controller.js
@@ -1,22 +1,47 @@
-var cricLive = new angular.module('cricLive',[]);
+var cricLive = new angular.module('cricLive',['ngSanitize']);
+ 
+ cricLive.filter('split', function() {
+        return function(input, splitChar, splitIndex) {
+            // do some bounds checking here to ensure it has that index
+            return input.split(splitChar)[splitIndex];
+        }
+    });
 
 cricLive.controller('MainController',['$scope','$http','$timeout',function($scope,$http,$timeout){
 
 
+	var audio = new Audio('../sound/click.wav');
+
+	$scope.alertToggle = true;
+	$scope.team1scoresep = [0,0];
+	$scope.team2scoresep = [0,0];
+
 	$scope.getMatches = function(){
 		 
 	 		$http.get('http://localhost:3001/getallmatches').success(function(response){
-	 			$scope.matches = response;
+	 			$scope.matches = []; //value,label
+	 			var tempmatch = response;
+	 			for(i=0;i<tempmatch.length;i++)
+	 			{
+	 				var match = tempmatch[i];
+		            var beg = match.indexOf(".com/");
+		            var curryear = new Date().getFullYear()-1;
+		            var end = match.indexOf(curryear);
+		            var matchname = match.substring(beg+5,end-1);
+	 				var temparr = {'id':match,'label':matchname};
+	 				$scope.matches.push(temparr);
+	 			}
 			
 		});
 	};
+
 
 	$scope.getMatchData = function(selectedmatchip){
 		var selectedmatch = {"matchurl":selectedmatchip};
 		(function tick() {
 	 		$http.post('http://localhost:3001/getmatchdata',selectedmatch).success(function(response){
- 			console.log("SERVER NE KYA BHEJA");
  			console.log(response);
+ 			console.log(response.bowling_table);
  			var inningsInfoObject = document.createElement('div');
  			inningsInfoObject.innerHTML = response.innings_info;
  			inningsInfoObject = $(inningsInfoObject);
@@ -25,7 +50,31 @@ cricLive.controller('MainController',['$scope','$http','$timeout',function($scop
  			scoreTableObject.innerHTML = response.score_table;
  			scoreTableObject = $(scoreTableObject);
 
- 			var recentOversObject = $(response.recent_overs);
+ 			var recentOversObject = document.createElement('div');
+ 			recentOversObject.innerHTML = response.recent_overs;
+ 			recentOversObject = $(recentOversObject);
+
+ 			$scope.recentovers = [];
+ 			try{
+ 				var nodes = recentOversObject.children(".recent-over");
+ 				console.log(nodes);
+ 				
+ 				for(i=0;i<nodes.length;i++)
+ 				{
+ 					var temp = [];
+ 					for(j=0;j<nodes[i].childElementCount;j++)
+ 					{
+ 						temp.push(nodes[i].children[j].innerHTML.trim());
+ 					}
+ 					$scope.recentovers.push(temp);
+ 					//console.log(temp);
+ 				}
+ 				console.log($scope.recentovers);
+ 			}
+ 			catch(err)
+ 			{
+
+ 			}
 
  			try
  			{
@@ -44,8 +93,21 @@ cricLive.controller('MainController',['$scope','$http','$timeout',function($scop
 
  			try{
 				$scope.team1score = inningsInfoObject.children(".team-1-name").children()[0].innerHTML;
-				if($scope.team1score == "")
+				if($scope.team1score == ""){
+					console.log("blank");
 					$scope.team1score="--/--";
+					$scope.team1scoresep=[0,0];
+					}
+				else{
+					console.log("not blank");
+					$scope.team1scoresep = $scope.team1score.split('/');
+					$scope.team1scoresep[0] = parseInt($scope.team1scoresep[0]);
+					$scope.team1scoresep[1] = parseInt($scope.team1scoresep[1].substr(0,2));
+					
+
+				}
+				console.log("score divide for team1",$scope.team1scoresep);
+
 
  			}
  			catch(err){
@@ -54,8 +116,18 @@ cricLive.controller('MainController',['$scope','$http','$timeout',function($scop
 
  			try{
 				$scope.team2score = inningsInfoObject.children(".team-2-name").children()[0].innerHTML;
-				if($scope.team2score == "")
+
+				if($scope.team2score == ""){
 					$scope.team2score="--/--";
+					$scope.team2scoresep =[0,0];
+					}
+				else{
+					$scope.team2scoresep = $scope.team2score.split('/');
+					$scope.team2scoresep[0] = parseInt($scope.team2scoresep[0]);
+					$scope.team2scoresep[1] = parseInt($scope.team2scoresep[1].substr(0,2));
+
+				}
+				console.log("score divide",$scope.team2scoresep);
 
  			}
  			catch(err){
@@ -90,7 +162,7 @@ cricLive.controller('MainController',['$scope','$http','$timeout',function($scop
 			{
 				$scope.batsmen2 = []; //runs,balls,4,6,StrikeRate
 				$scope.batsmen2.push(scoreTableObject.find(".batsmen").find(".row2").children()[0].children[0].text);
-				$scope.batsmen2.push(scoreTableObject.find(".batsmen").find(".row2").children()[2].innerHTML);
+				$scope.batsmen2.push(scoreTableObject.find(".batsmen").find(".row2").children()[1].innerHTML);
 				$scope.batsmen2.push(scoreTableObject.find(".batsmen").find(".row2").children()[2].innerHTML);
 				$scope.batsmen2.push(scoreTableObject.find(".batsmen").find(".row2").children()[3].innerHTML);
 				$scope.batsmen2.push(scoreTableObject.find(".batsmen").find(".row2").children()[4].innerHTML);
@@ -109,7 +181,7 @@ cricLive.controller('MainController',['$scope','$http','$timeout',function($scop
 				$scope.bowlerStats = []; //multiple bowlers can be present
 				var bowlerNodes = scoreTableObject.find(".bowlers").children()[1].rows.length;
 				for(i=0; i<bowlerNodes; i+=1) {
-				    var tempbowler=[]; //overs,maiden,runs,wicket,ecnonomy
+				    var tempbowler=[]; //name,overs,maiden,runs,wicket,ecnonomy
 				 	tempbowler.push(scoreTableObject.find(".bowlers").children()[1].children[i].children[0].children[0].text);
 				 	tempbowler.push(scoreTableObject.find(".bowlers").children()[1].children[i].children[1].innerHTML); 
 				 	tempbowler.push(scoreTableObject.find(".bowlers").children()[1].children[i].children[2].innerHTML); 
@@ -124,15 +196,38 @@ cricLive.controller('MainController',['$scope','$http','$timeout',function($scop
 			{
 				$scope.bowlerStats = []; //multiple bowlers can be present				
 			}
-
-
-
-			// $scope.batsmen2=
 			 $timeout(tick, 10000);
 			
 		});
 	 		})();
 	};
+
+	
+
+		$scope.$watch('team1scoresep', function(newValue,oldValue){
+			if(!angular.isUndefined(newValue) && !angular.isUndefined(oldValue)){
+				console.log("oldValue Team1",oldValue);
+				console.log("newValue Team1",newValue);
+				if((newValue[0] - oldValue[0] == 6)||(newValue[0] - oldValue[0] == 5)||(newValue[0] - oldValue[0] == 1)||(newValue[0] - oldValue[0] == 6)||(newValue[0] - oldValue[0] == 4)){
+					if($scope.alertToggle)audio.play();
+				}
+				if(newValue[1] - oldValue[1] == 1)
+					if($scope.alertToggle)audio.play();
+			}
+		});
+		$scope.$watch('team2scoresep', function(oldValue,newValue){
+			if(!angular.isUndefined(newValue) && !angular.isUndefined(oldValue)){
+				console.log("oldValue Team2",oldValue);
+				console.log("newValue Team2",newValue);
+				if((newValue[0] - oldValue[0] == 6)||(newValue[0] - oldValue[0] == 5)||(newValue[0] - oldValue[0] == 1)||(newValue[0] - oldValue[0] == 6)||(newValue[0] - oldValue[0] == 4)){
+					if($scope.alertToggle)audio.play();
+				}
+				if(newValue[1] - oldValue[1] == 1)
+					if($scope.alertToggle)audio.play();
+			}
+		});
+
+		
 
 }]);
 
--- js/main.js
+++ js/main.js
@@ -67,15 +67,27 @@ function getMatchData(matchurl,callback) {
                 var inningsinfo = [];
                 var scoretable = [];
                 var recentovers = [];
+                var bowlingtable = [];
      
                 $('.innings-information').each(function () { inningsinfo.push($(this).html()); });
                 $('.score-table').each(function () { scoretable.push($(this).html()); });
                 $('.recent-overs').each(function () { recentovers.push($(this).html()); });
+
+                var nav = $('.tab-full-scorecard')[0]; // obtain reference to the underlying DOMElement
+        
+               // click anchor link to "Modular code"
+                var ev = document.createEvent("MouseEvents");
+                ev.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+                nav.dispatchEvent(ev);
+
+                $('.bowling-table').each(function () { bowlingtable.push($(this).html()); });
+
                 //console.log("inningsinfo",inningsinfo);
 
                 return{innings_info:inningsinfo,
                         score_table:scoretable,
-                        recent_overs:recentovers};
+                        recent_overs:recentovers,
+                        bowling_table:bowlingtable};
               }, function (err,result) {
                 scrapedMatchData = result;
                 browser.exit();
@@ -107,43 +119,5 @@ app.post('/getmatchdata', function(req, res) {
 });
 
 
-menu.append(new gui.MenuItem({
-	label: 'File',
-	submenu: new gui.Menu()
-}));
-
-menu.items[0].submenu.append(new gui.MenuItem({
-	label: 'New',
-	click: function () {
-		gui.Window.open('index.html');
-	}
-}));
-menu.items[0].submenu.append(new gui.MenuItem({
-	type: 'separator'
-}));
-menu.items[0].submenu.append(new gui.MenuItem({
-	label: 'Close',
-	click: function () {
-		gui.Window.get().close();
-	}
-}));
-
-gui.Window.get().menu = menu;
-
-function clickInput(id) {
-	var event = document.createEvent('MouseEvents');
-	event.initMouseEvent('click');
-	document.getElementById(id).dispatchEvent(event);
-}
-
-document.addEventListener('keyup', function (e) {
-	if (e.keyCode == 'O'.charCodeAt(0) && e.ctrlKey) {
-		clickInput('open');
-	} else if (e.keyCode == 'S'.charCodeAt(0) && e.ctrlKey) {
-		clickInput('save');
-	} else if (e.keyCode == 'N'.charCodeAt(0) && e.ctrlKey) {
-	gui.Window.open('notes.html');
-    }
-});
 
 app.listen(3001);
